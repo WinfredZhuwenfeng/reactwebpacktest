028ee1463476fd3b1a1ee036c4a0603363cdcb85
WinfredZhuwenfeng <1419657141@qq.com>
2019-04-21 16:48:01 +0800

    webpackready

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,32 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-  template : `${__dirname}/app/index.html`,
-  filename : 'index.html',
-  inject : 'body'
+  template: `${__dirname}/app/index.html`,
+  filename: 'index.html',
+  inject: 'body'
 })
 module.export = {
-  entry : 
+  entry: [
+    './app/index.js'
+  ],
+  output: {
+    path: `${__dirname}/dist`,
+    filename: 'index_bundle.js'
+  },
+  moudle: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_module/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        } 
+      }
+    ],
+  },
+  devServer: {
+    inline: true,
+    port: 8008
+  },
+  plugins: [HtmlWebpackPluginConfig],
 }
